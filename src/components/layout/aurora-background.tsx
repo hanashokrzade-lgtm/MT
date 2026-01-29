3af8fc8574a629ae7fbbfbb9dd01615048009d69
Hana Shokrzade <hanashokrzade@gmail.com>
2026-01-29 18:17:52 +0000

    حاله های رنگی بیشتری داشته باشه و در رنگ های متنوع باشه

--- src/components/layout/aurora-background.tsx
+++ src/components/layout/aurora-background.tsx
@@ -10,7 +10,15 @@ export function AuroraBackground() {
             "bg-background"
         )}
     >
-        <div className="absolute inset-0 bg-[radial-gradient(circle_at_center,hsl(var(--primary)/0.1),transparent_40%),radial-gradient(circle_at_1px_1px,hsl(var(--border))_1px,transparent_0)] bg-[length:100%_100%,2rem_2rem]"></div>
+        {/* Dot Grid */}
+        <div className="absolute inset-0 bg-[radial-gradient(circle_at_1px_1px,hsl(var(--border)/0.5)_1px,transparent_0)] bg-[length:2rem_2rem]"></div>
+        
+        {/* Color Halos */}
+        <div className="absolute inset-0 pointer-events-none">
+            <div className="absolute top-[-20%] right-[0%] h-[400px] w-[400px] rounded-full bg-primary/10 blur-3xl lg:h-[600px] lg:w-[600px] lg:top-[-30%] lg:right-[-5%]"></div>
+            <div className="absolute bottom-[-10%] left-[-10%] h-[350px] w-[350px] rounded-full bg-fuchsia-500/10 blur-3xl lg:h-[500px] lg:w-[500px] lg:bottom-[-20%] lg:left-[-5%]"></div>
+            <div className="absolute bottom-[0%] right-[20%] h-[300px] w-[500px] rounded-full bg-sky-500/10 blur-3xl lg:h-[400px] lg:w-[700px] lg:bottom-[-10%]"></div>
+        </div>
     </div>
   )
 }
